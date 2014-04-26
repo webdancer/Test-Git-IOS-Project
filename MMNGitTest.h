336dd044f9f4dc08c4ad88527702d02c921d20c2
Murdo MacNab <mail2murdo@gmail.com>
2014-04-26 18:45:28 +0100

    Second Commit

---
+++ MMNGitTest.h
@@ -0,0 +1,13 @@
+//
+//  MMNGitTest.h
+//  Git test
+//
+//  Created by Murdo MacNab on 26/04/2014.
+//  Copyright (c) 2014 Murdo MacNab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMNGitTest : NSObject
+
+@end
